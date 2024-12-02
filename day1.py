3c208c9c259965d15ff8f22fe40920baf7c75b40
qzhu305 <qzhu305@gatech.edu>
2024-12-01 18:39:50 -0800

    day1

---
+++ day1.py
@@ -0,0 +1,87 @@
+import util
+import collections
+
+"""
+Algo key point:
+1. sort algo: using the native Python sort. It takes O(nlogn). Need know write
+from scratch on Merge-sort, Quick-sort
+2. hash counter: O(n), get counts on listB, then A get frequency is O(1)
+"""
+
+def get_input_data():
+    """Fetches the input data for the Advent of Code puzzle.
+
+    Logs into the Advent of Code website using the session from 'util', and fetches
+    the puzzle input for day 1.
+
+    Returns:
+        str: The raw puzzle input as a string.
+    """
+    aoc_session = util.login_advent_of_code()
+    if aoc_session:
+        day = 1
+        input_url = f'https://adventofcode.com/2024/day/{day}/input'
+        input_response = aoc_session.get(input_url)
+        puzzle_input = input_response.text.strip()
+        return puzzle_input
+    else:
+        return ""
+
+def parse_input():
+    """Parses the raw puzzle input into two lists of integers.
+
+    Splits the input by lines, then splits each line into two integers, which are added
+    to separate lists.
+
+    Returns:
+        tuple: A tuple containing two lists of integers (list_a, list_b).
+    """
+    puzzle_input = get_input_data()
+    list_a = []
+    list_b = []
+    for line in puzzle_input.split("\n"):
+        numbers = line.strip().split("   ")  # Assuming three spaces as delimiter
+        list_a.append(int(numbers[0]))
+        list_b.append(int(numbers[1]))
+    return list_a, list_b
+
+def get_total_distance(a, b):
+    """Calculates the total absolute distance between corresponding elements of two lists.
+
+    The lists are first sorted, then the absolute differences between each pair of elements
+    are summed.
+
+    Args:
+        a (list of int): First list of integers.
+        b (list of int): Second list of integers.
+
+    Returns:
+        int: The total absolute distance between the two lists.
+    """
+    res = 0
+    a_sorted = sorted(a)
+    b_sorted = sorted(b)
+    for i in range(len(a_sorted)):
+        res += abs(a_sorted[i] - b_sorted[i])
+    return res
+
+def get_similarity_score(a, b):
+    """Calculates a similarity score between two lists.
+
+    For each number in list 'a', counts how many times it appears in list 'b', and multiplies
+    the number by that count.
+
+    Args:
+        a (list of int): First list of integers.
+        b (list of int): Second list of integers.
+
+    Returns:
+        int: The similarity score.
+    """
+    b_counts = collections.Counter(b)
+    return sum(num * b_counts[num] for num in a)
+
+if __name__ == "__main__":
+    list_a, list_b = parse_input()
+    print(get_total_distance(list_a, list_b))
+    print(get_similarity_score(list_a, list_b))
